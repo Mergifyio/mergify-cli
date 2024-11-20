04f6e5435454d2392b7d8e6690ff9c58888cd98e
Mehdi ABAAKOUK <sileht@sileht.net>
2024-11-20 08:45:34 +0100

    fix(stack-setup): put hooks at the right place
    This adds test coverage to stack_setup to ensure we don't break this
    again.
    
    Change-Id: Iffd484d926e55f5136d19e02f203ca3aab82b438

---
+++ mergify_cli/tests/stack/test_setup.py
@@ -0,0 +1,20 @@
+import pathlib
+import typing
+
+import pytest
+
+from mergify_cli.stack import setup
+from mergify_cli.tests import utils as test_utils
+
+
+async def test_setup(
+    git_mock: test_utils.GitMock,
+    tmp_path: pytest.TempdirFactory,
+) -> None:
+    hooks_dir = typing.cast(pathlib.Path, tmp_path) / ".git" / "hooks"
+    hooks_dir.mkdir(parents=True)
+    git_mock.mock("rev-parse", "--git-path", "hooks", output=str(hooks_dir))
+    await setup.stack_setup()
+
+    hook = hooks_dir / "commit-msg"
+    assert hook.exists()
