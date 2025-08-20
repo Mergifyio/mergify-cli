9f31461420940b2708de29fc8291891760f22caf
Mehdi ABAAKOUK <sileht@sileht.net>
2025-08-20 14:42:23 +0200

    chore(jenkins): add pipeline name (#749)

--- mergify_cli/ci/detector.py
+++ mergify_cli/ci/detector.py
@@ -24,8 +24,11 @@ def get_ci_provider() -> CIProviderT | None:
 
 
 def get_pipeline_name() -> str | None:
-    if get_ci_provider() == "github_actions":
-        return os.getenv("GITHUB_WORKFLOW")
+    match get_ci_provider():
+        case "github_actions":
+            return os.getenv("GITHUB_WORKFLOW")
+        case "jenkins":
+            return os.getenv("JOB_NAME")
 
     return None
 
